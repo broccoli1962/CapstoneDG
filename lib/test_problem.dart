5638a0d97c139f2df4864c9465189abe5f2fd520
broccoli <aq0011@naver.com>
2024-06-05 19:13:58 +0900

    시험문제 텍스트필드 수정
    //

--- lib/test_problem.dart
+++ lib/test_problem.dart
@@ -17,26 +17,69 @@ class Test_in extends StatefulWidget {
   State<Test_in> createState() => _Test_inState();
 }
 
+
 class _Test_inState extends State<Test_in> {
   final getapi = jdoodleAPI(ClientId: '', ClientSecret: '');
   String _output = '';
 
+  late Problem_t view;
+
+  //텍스트 컨트롤
+  late TextEditingController _controller;
+  late FocusNode _focusNode;
+
+  @override
+  void initState(){
+    super.initState();
+    view = Problem_t.get(widget.pg, widget.number);
+    _controller = TextEditingController(text: view.contents);
+    _focusNode = FocusNode();
+
+    _focusNode.addListener(() {
+      if(!_focusNode.hasFocus) {
+        _changeTextFieldValue(_controller.text);
+      }
+    });
+  }
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _changeTextFieldValue(String newValue){
+    setState(() {
+      _controller.text = newValue;
+    });
+  }
+
+  //메인 화면 구성
   @override
   Widget build(BuildContext context) {
     final Size cSize = MediaQuery.of(context).size;
     final Problem_t view = Problem_t.get(widget.pg, widget.number);
 
-    //초기값
-    String tt = view.contents;
-    TextEditingController inputController = TextEditingController(text: tt);
-    // final CustomTagController controller = CustomTagController();
 
-    void dispose(){
-      inputController.dispose();
-      super.dispose();
+    void explain(context){
+      showDialog(context: context, builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('해설'),
+          content: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Text(view.hint, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          ),
+          insetPadding: const EdgeInsets.all(10),
+          actions: [
+            OutlinedButton(onPressed: (){Navigator.of(context).pop(); }, child: Text('ok')),
+          ],
+        );
+      }
+      );
     }
 
-
+//코딩 api
     Future<void> _executeCode() async {
       final code = view.contents;
       final language = 'c';
@@ -62,6 +105,10 @@ class _Test_inState extends State<Test_in> {
             Navigator.pop(context);
           }, child: Text('돌아가기')),
         ],
+        leading: Padding(
+          padding: EdgeInsets.all(2.0),
+          child: TextButton(child: Text('출력하기'), onPressed: (){}),
+        ),
         centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 0,
@@ -103,10 +150,13 @@ class _Test_inState extends State<Test_in> {
               padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
               child: TextField(
                 maxLines: 22,
+                controller: _controller,
+                focusNode: _focusNode,
               ),
             )
           ],
         ),
+        floatingActionButton: FloatingActionButton(onPressed: () {explain(context);}, child: Text('문제풀이'),),
     );
   }
 }
