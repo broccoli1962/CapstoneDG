b91edc9d753e999b84252abe1abd1051e1532a5d
broccoli1962 <aq0011@naver.com>
2024-08-17 13:36:39 +0900

    TEST DB 연결
    TEST DB 연결

--- lib/practice_problem.dart
+++ lib/practice_problem.dart
@@ -24,9 +24,16 @@ class _Practice_inState extends State<Practice_in> {
   String hint = "";
 
 //data 가져오기
-  Future<void> _initData() async{
+  Future<void> _initData() async {
     FirebaseFirestore db = FirebaseFirestore.instance;
-    final ref = db.collection("practice").withConverter(fromFirestore: (snapshot, _) => FireData.fromJson(snapshot.data()!), toFirestore: (FireData product, _) => product.toJson()).where("chapter", isEqualTo: widget.pg).where("number", isEqualTo: widget.number);
+    final ref = db
+        .collection("practice")
+        .withConverter(
+            fromFirestore: (snapshot, _) =>
+                FireData_P.fromJson(snapshot.data()!),
+            toFirestore: (FireData_P product, _) => product.toJson())
+        .where("chapter", isEqualTo: widget.pg)
+        .where("number", isEqualTo: widget.number);
     final docSnap = await ref.get();
     final inData = docSnap.docs.first.data();
 
@@ -49,26 +56,25 @@ class _Practice_inState extends State<Practice_in> {
 
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading:false,
+        automaticallyImplyLeading: false,
         title: Text(
-          title, textAlign: TextAlign.center,
+          title,
+          textAlign: TextAlign.center,
           style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
         ),
         actions: [
-          TextButton(onPressed: (){
-            Navigator.pop(context);
-          }, child: Text('돌아가기')),
+          TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('돌아가기')),
         ],
         centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 0,
         bottom: const PreferredSize(
           preferredSize: Size.fromHeight(1),
-          child: Divider(
-            height: 2,
-            thickness: 1,
-              color: Color(0xFF005629)
-          ),
+          child: Divider(height: 2, thickness: 1, color: Color(0xFF005629)),
         ),
       ),
       body: Column(
@@ -78,12 +84,16 @@ class _Practice_inState extends State<Practice_in> {
               children: [
                 Container(
                   padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                  height: cSize.height*0.55,
+                  height: cSize.height * 0.55,
                   width: double.infinity,
                   color: Colors.white,
                   child: SingleChildScrollView(
-                      child: Text(contents, style:TextStyle(color: Colors.black, fontSize: 20,fontWeight: FontWeight.bold, letterSpacing: 2.0))
-                  ),
+                      child: Text(contents,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 2.0))),
                 )
               ],
             ),
@@ -94,12 +104,16 @@ class _Practice_inState extends State<Practice_in> {
           ),
           Container(
             padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            height: cSize.height*0.218,
+            height: cSize.height * 0.218,
             width: double.infinity,
             color: Colors.white,
             child: SingleChildScrollView(
-                child: Text(hint, style:const TextStyle(color: Colors.black, fontSize: 17,fontWeight: FontWeight.bold, letterSpacing: 2.0))
-            ),
+                child: Text(hint,
+                    style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 2.0))),
           ),
         ],
       ),
--- lib/test_problem.dart
+++ lib/test_problem.dart
@@ -1,5 +1,6 @@
-import 'dart:core';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:untitled/utils/database.dart';
 import 'package:untitled/utils/util.dart';
 import 'utils/http_api.dart';
 
@@ -17,76 +18,137 @@ class Test_in extends StatefulWidget {
   State<Test_in> createState() => _Test_inState();
 }
 
-
 class _Test_inState extends State<Test_in> {
+  //jdoodle api
   final getapi = jdoodleAPI(ClientId: '', ClientSecret: '');
   String _output = '';
 
   late Problem_t view;
   late String _contents;
 
-  @override
-  void initState(){
-    super.initState();
-    view = Problem_t.get(widget.pg, widget.number);
+  //코딩 api
+  Future<void> _executeCode() async {
+    final code = view.contents;
+    final language = 'c';
+
+    try {
+      final output = await getapi.executeCode(code, language);
+      setState(() {
+        _output = '출력 결과 : $output';
+      });
+    } on Exception catch (e) {
+      setState(() {
+        _output = 'Error:$e';
+      });
+    }
   }
 
-  void _onChangedText(String newValue){
+  //텍스트 실시간 변경
+  void _onChangedText(String newValue) {
     setState(() {
       _contents = newValue;
     });
   }
 
+  //db
+  String title = "";
+  String testCase = "";
+  String rtestCase = "";
+  String contents = "";
+  String myAnswer = "";
+  String hint = "";
+  String answer = "";
+
+  Future<void> _initData() async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    final ref = db
+        .collection("test")
+        .withConverter(
+            fromFirestore: (snapshot, _) =>
+                FireData_T.fromJson(snapshot.data()!),
+            toFirestore: (FireData_T product, _) => product.toJson())
+        .where("chapter", isEqualTo: widget.pg)
+        .where("number", isEqualTo: widget.number);
+    try{
+      final docSnap = await ref.get();
+      if(docSnap.docs.isNotEmpty){
+        final inData = docSnap.docs.first.data();
+
+        setState(() {
+          title = inData.title.replaceAll("\\n", "\n");
+          testCase = inData.testCase.replaceAll("\\n", "\n");
+          rtestCase = inData.rtestCase.replaceAll("\\n", "\n");
+          contents = inData.context.replaceAll("\\n", "\n");
+          hint = inData.hint.replaceAll("\\n", "\n");
+          answer = inData.answer;
+        });
+      }else{
+        setState(() {
+          title = "데이터 불러오기 실패";
+          testCase = "데이터 불러오기 실패";
+          rtestCase = "데이터 불러오기 실패";
+          contents = "데이터 불러오기 실패";
+          hint = "데이터 불러오기 실패";
+          answer = "데이터 불러오기 실패";
+        });
+      }
+    }catch(e){
+      print('error : $e');
+    }
+  }
+
   //메인 화면 구성
+  void explain(context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('해설'),
+            content: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                hint,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            insetPadding: const EdgeInsets.all(10),
+            actions: [
+              OutlinedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('ok')),
+            ],
+          );
+        },
+    );
+  }
+
+  @override
+  void initState() {
+    _initData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size cSize = MediaQuery.of(context).size;
-    final Problem_t view = Problem_t.get(widget.pg, widget.number);
-
-
-    void explain(context){
-      showDialog(context: context, builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('해설'),
-          content: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: Text(view.hint, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-          ),
-          insetPadding: const EdgeInsets.all(10),
-          actions: [
-            OutlinedButton(onPressed: (){Navigator.of(context).pop(); }, child: Text('ok')),
-          ],
-        );
-      }
-      );
-    }
-
-//코딩 api
-    Future<void> _executeCode() async {
-      final code = view.contents;
-      final language = 'c';
-
-      try{
-        final output = await getapi.executeCode(code, language);
-        setState(() {
-          _output = '출력 결과 : $output';
-        });
-      } on Exception catch(e){
-        setState(() {
-          _output = 'Error:$e';
-        });
-      }
-    }
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        automaticallyImplyLeading:false,
-        title: Text(view.title, style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
+        automaticallyImplyLeading: false,
+        title: Text(
+          title,
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
         actions: [
-          TextButton(onPressed: (){
-            Navigator.pop(context);
-          }, child: const Text('돌아가기')),
+          TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('돌아가기')),
         ],
         leading: Padding(
           padding: const EdgeInsets.all(2.0),
@@ -97,81 +159,77 @@ class _Test_inState extends State<Test_in> {
         elevation: 0,
         bottom: const PreferredSize(
           preferredSize: Size.fromHeight(1),
-          child: Divider(
-            height: 2,
-            thickness: 1,
-              color: Color(0xFF005629)
-          ),
+          child: Divider(height: 2, thickness: 1, color: Color(0xFF005629)),
         ),
       ),
-
-      body:
-        Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: cSize.height * 0.17,
-              padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
-              decoration: const BoxDecoration(color: Colors.white),
-              child: SingleChildScrollView( // SingleChildScrollView로 감싸기
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      '입력 값',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.left,
-                    ),
-                    Text(view.testCase),
-                  ],
-                ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: cSize.height * 0.17,
+            padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+            decoration: const BoxDecoration(color: Colors.white),
+            child: SingleChildScrollView(
+              // SingleChildScrollView로 감싸기
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    '입력 값',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.left,
+                  ),
+                  Text(testCase),
+                ],
               ),
             ),
-
-            const Divider(
-              color: Colors.black,
-              height: 0.01,
-            ),
-
-            Container(
-              width: double.infinity,
-              height: cSize.height * 0.17,
-              padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-              decoration: const BoxDecoration(color: Colors.white),
-              child: SingleChildScrollView( // SingleChildScrollView로 감싸기
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      '출력 값',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.left,
-                    ),
-                    Text(view.rtestCase2),
-                  ],
-                ),
+          ),
+          const Divider(
+            color: Colors.black,
+            height: 0.01,
+          ),
+          Container(
+            width: double.infinity,
+            height: cSize.height * 0.17,
+            padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+            decoration: const BoxDecoration(color: Colors.white),
+            child: SingleChildScrollView(
+              // SingleChildScrollView로 감싸기
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    '출력 값',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.left,
+                  ),
+                  Text(rtestCase),
+                ],
               ),
             ),
-
-            const Divider(
-              color: Colors.black,
-              height: 0.01,
-            ),
-
-            Container(
+          ),
+          const Divider(
+            color: Colors.black,
+            height: 0.01,
+          ),
+          Container(
               width: double.infinity,
-              height: cSize.height*0.55651,
+              height: cSize.height * 0.55651,
               decoration: const BoxDecoration(color: Colors.white),
               padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
               child: CustomTextField(
                 onTextChanged: _onChangedText,
-                initialText: view.contents,
+                initialText: contents,
                 maxLines: 23,
-              )
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(onPressed: () {explain(context);}, child: Text('문제풀이'),),
+              ))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          explain(context);
+        },
+        child: Text('문제풀이'),
+      ),
     );
   }
 }
